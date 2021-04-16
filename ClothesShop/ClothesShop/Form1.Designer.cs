cc04d5861d4ef568f36558ece659855fa0c62ceb
Anly01 <anlysurya01@gmail.com>
2021-04-16 19:03:40 +0700

    menghapus button

--- ClothesShop/ClothesShop/Form1.Designer.cs
+++ ClothesShop/ClothesShop/Form1.Designer.cs
@@ -29,24 +29,24 @@ namespace ClothesShop
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buy_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // buy_btn
             // 
-            this.button1.Location = new System.Drawing.Point(70, 47);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buy_btn.Location = new System.Drawing.Point(12, 12);
+            this.buy_btn.Name = "buy_btn";
+            this.buy_btn.Size = new System.Drawing.Size(81, 35);
+            this.buy_btn.TabIndex = 0;
+            this.buy_btn.Text = "BUY";
+            this.buy_btn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buy_btn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +55,7 @@ namespace ClothesShop
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buy_btn;
     }
 }
 
