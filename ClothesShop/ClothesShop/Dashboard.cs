4d04fdb9a2cb3781545ce766741de44a07dabfad
vendryan <laboyng@gmail.com>
2021-04-20 17:03:19 +0700

    added form admin

---
+++ ClothesShop/ClothesShop/Dashboard.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sugi_Huang
+ * Date: 20/04/2021
+ * Time: 16:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClothesShop
+{
+	/// <summary>
+	/// Description of Dashboard.
+	/// </summary>
+	public partial class Dashboard : Form
+	{
+		public Dashboard()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
