cbf282235807b3aec69b7ec4c28b032420b0b3a5
Anly01 <anlysurya01@gmail.com>
2021-04-23 12:45:33 +0700

    menambahkan peopleform untuk test DB, dan menambahkan database sqlite ClothesShopDB

---
+++ ClothesShop/ClothesShop/peopleform.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace ClothesShop
+{
+    partial class peopleform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listPeopleListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(30, 62);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(154, 26);
+            this.firstNameText.TabIndex = 0;
+            this.firstNameText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(30, 107);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(154, 26);
+            this.lastNameText.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listPeopleListBox
+            // 
+            this.listPeopleListBox.FormattingEnabled = true;
+            this.listPeopleListBox.ItemHeight = 20;
+            this.listPeopleListBox.Location = new System.Drawing.Point(272, 62);
+            this.listPeopleListBox.Name = "listPeopleListBox";
+            this.listPeopleListBox.Size = new System.Drawing.Size(224, 224);
+            this.listPeopleListBox.TabIndex = 4;
+            // 
+            // peopleform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 383);
+            this.Controls.Add(this.listPeopleListBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.firstNameText);
+            this.Name = "peopleform";
+            this.Text = "peopleform";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listPeopleListBox;
+    }
+}
---
+++ ClothesShop/ClothesShop/peopleform.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClothesShop
+{
+    public partial class peopleform : Form
+    {
+        List<PersonModel> people = new List<PersonModel>();
+        public peopleform()
+        {
+            InitializeComponent();
+
+            LoadPeopleList();
+        }
+
+        private void LoadPeopleList()
+        {
+            people = SqliteDataAccess.LoadPeople();
+
+            WireUpPeopleList();
+        }
+
+        private void WireUpPeopleList()
+        {
+            listPeopleListBox.DataSource = null;
+            listPeopleListBox.DataSource = people;
+            listPeopleListBox.DisplayMember = "FullName";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadPeopleList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PersonModel p = new PersonModel();
+            p.FirstName = firstNameText.Text;
+            p.LastName = lastNameText.Text;
+
+            SqliteDataAccess.SavePerson(p);
+            firstNameText.Text = "";
+            lastNameText.Text = "";
+
+        }
+    }
+}
