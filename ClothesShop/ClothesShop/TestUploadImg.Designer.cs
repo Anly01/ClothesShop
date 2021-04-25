975e2a4c72aced221d0a19dd3007fc2e0bc8f0e9
vendryan <laboyng@gmail.com>
2021-04-25 14:06:31 +0700

    Test upload image

--- ClothesShop/ClothesShop/TestUploadImg.Designer.cs
+++ ClothesShop/ClothesShop/TestUploadImg.Designer.cs
@@ -38,6 +38,8 @@ namespace ClothesShop
 		{
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -52,7 +54,7 @@ namespace ClothesShop
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(52, 152);
+			this.button1.Location = new System.Drawing.Point(51, 132);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(182, 35);
 			this.button1.TabIndex = 1;
@@ -60,11 +62,33 @@ namespace ClothesShop
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.Button1Click);
 			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(51, 173);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(181, 33);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Save to DB";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(51, 216);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(180, 34);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Load from DB";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
 			// TestUploadImg
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.pictureBox1);
 			this.Name = "TestUploadImg";
@@ -72,6 +96,8 @@ namespace ClothesShop
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.PictureBox pictureBox1;
 	}
